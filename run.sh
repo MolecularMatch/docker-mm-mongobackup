731da2dc4ece51402d06a8f7cdc71e7056c7cd98
Ryan Smith <smithrp@gmail.com>
2015-07-01 10:11:09 -0700

    remove the backup file

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ BACKUPFILE=/tmp/$BACKUPNAME.tar.gz
 mongodump --host $HOST --port $PORT --db $DBNAME --username $USERNAME --password $PASSWORD --out $BACKUPDIR
 tar -cvzf $BACKUPFILE $BACKUPDIR
 aws s3 cp $BACKUPFILE s3://mms3backupbucket/mongodb/
+
+rm $BACKUPFILE
